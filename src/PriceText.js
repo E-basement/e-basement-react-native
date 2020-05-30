3844784fce9b06f2869ecbb0da4c195c8f81cb64
jBosak <jbosak98@gmail.com>
2020-05-30 23:30:36 +0200

    added style as props to priceText

--- src/PriceText.js
+++ src/PriceText.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text } from "react-native";
 import { FONT_COLOR, FONT_FAMILY } from "./Constants";
 
-const PriceText = ({ price }) => {
+const PriceText = ({ price, style }) => {
   const { amount, currency } = price;
   return (
     <Text
@@ -11,6 +11,7 @@ const PriceText = ({ price }) => {
         fontSize: 20,
         fontFamily: FONT_FAMILY,
         color: FONT_COLOR,
+        ...style
       }}
     >
       {(amount / 100).toFixed(2) + " " + currency}
